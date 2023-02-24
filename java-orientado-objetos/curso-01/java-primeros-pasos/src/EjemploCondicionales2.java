97b76f32360a8dfc6395a95dc15cf2325867116e
isbaruc <isbaruc@proton.me>
2023-02-24 00:36:13 -0600

    agrega modificaciones al archivo

--- java-orientado-objetos/curso-01/java-primeros-pasos/src/EjemploCondicionales2.java
+++ java-orientado-objetos/curso-01/java-primeros-pasos/src/EjemploCondicionales2.java
@@ -8,7 +8,13 @@ public class EjemploCondicionales2 {
 		int cantidadPersonas = 2; //Hacemos más específica la variable  (Autoexplicativa)
 		
 		//boolean esPareja = true; //Los valores de boolean solo pueden ser true o false
-		boolean esPareja = cantidadPersonas > 1;
+		boolean esPareja = cantidadPersonas > 1; //Aplicamos la condición dentro de la variable y devolverá true o false dependiendo de la variable evaluada
+		
+		//Una alternativa a la condiconal sería
+		//boolean puedeEntrar = edad >= 18 && esPareja;
+		//if (puedeEntrar) {
+		//	System.out.println("Usted puede entrar");
+		//}
 		
 		if (edad >= 18 || esPareja) { //Los valores boolean no necesitan ser evaluados
 			System.out.println("Usted puede entrar");

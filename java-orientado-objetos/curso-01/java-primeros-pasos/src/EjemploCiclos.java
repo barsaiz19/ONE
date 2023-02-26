2a5d467301fe95da341f6419e1aac7fe0b8af294
isbaruc <isbaruc@proton.me>
2023-02-25 20:44:22 -0600

    Agrega archivo de ejemplo clase ciclos

---
+++ java-orientado-objetos/curso-01/java-primeros-pasos/src/EjemploCiclos.java
@@ -0,0 +1,13 @@
+
+public class EjemploCiclos {
+	public static void main(String[] args) {
+		//Imprimir numeros del 1 al 10
+		int contador = 0;
+		
+		while (contador <= 10) {
+			System.out.println(contador);
+			//contador = contador + 1; //Esta expresion puede ser cosiderada anti patron
+			contador += 1; //Alternativa a linea interior para incremento en la variable
+		}
+	}
+}

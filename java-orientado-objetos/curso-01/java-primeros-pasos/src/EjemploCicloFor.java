7ffd2d87c7cd6fdbaea855ae3f299464fc363fe7
isbaruc <isbaruc@proton.me>
2023-02-25 21:06:55 -0600

    Agrega archivo ejemplo clase ciclos for

---
+++ java-orientado-objetos/curso-01/java-primeros-pasos/src/EjemploCicloFor.java
@@ -0,0 +1,11 @@
+
+public class EjemploCicloFor {
+
+	public static void main(String[] args) {
+		//Imprime numeros del 1 al 10 usando FOR
+		
+		for (int contador = 0; contador <= 10; contador++) {
+			System.out.println(contador);
+		}
+	}
+}
